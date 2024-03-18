44a92866df2279860af3842529052df062c036e6
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-03-18 20:50:16 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,9 @@
 # متطلبات التنصيب #
 OWNER = ["EU_TM"]
-OWNER_NAME = "𝗸𝗮𝗯𝗼𝗼𝘀 𝗯𝗮𝘀𝗵𝗮"
+OWNER_NAME = "Lotus"
 BOT_TOKEN = "6837293019:AAHbX5L2i8RyUvYvlwEcuEoEC5ZscUZvUOE"
 DATABASE = "mongodb+srv://rachel105293:qaB7BgWAUZsQzsZD@cluster0.amqwq3r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-CHANNEL = "https://t.me/KK_DX"
-GROUP = "https://t.me/HHYTT"
-VIDEO = "https://t.me/KA_XB/2461"
-LOGS = "EGYPT_Soupr"
-
+CHANNEL = "https://t.me/l2_2Y"
+GROUP = "https://t.me/jx_xll"
+VIDEO = "https://t.me/l2_2Y"
+LOGS = "Tm_Lotus"
