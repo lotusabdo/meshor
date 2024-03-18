d7f154493ff0ba0c18d05eff9c863fa03902255b
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-03-18 20:17:53 +0200

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["Z_C_T", "BMBB4"]
+OWNER_NAME = "𝗸𝗮𝗯𝗼𝗼𝘀 𝗯𝗮𝘀𝗵𝗮"
+BOT_TOKEN = ""
+DATABASE = ""
+CHANNEL = "https://t.me/KK_DX"
+GROUP = "https://t.me/HHYTT"
+VIDEO = "https://t.me/KA_XB/2461"
+LOGS = "EGYPT_Soupr"
+
