58fc2d3512975a7acb7832ea57e454920d61345a
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-03-21 12:20:49 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -2,7 +2,7 @@
 OWNER = ["EU_TM"]
 OWNER_NAME = "𖥻 𝐔𝐑 , 𝐅𝐚𝐕 𝐀𝐛𝐃𝐎𝐨 -"
 BOT_TOKEN = "6837293019:AAHbX5L2i8RyUvYvlwEcuEoEC5ZscUZvUOE"
-DATABASE = "mongodb+srv://daisy610576:9pfXvorsdR2j7Ogv@cluster0.xv0lajq.mongodb.net/?retryWrites=true&w=majority"
+DATABASE = "mongodb+srv://bailey965141:kXpHuaPQprpi0S4z@cluster0.olihsnc.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/l2_2Y"
 GROUP = "https://t.me/jx_xll"
 VIDEO = "https://t.me/l2_2Y"
